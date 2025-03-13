2a43d1784bf560e1aea358a1a04e9fad21c1cfba
Tracy Hotchkiss <trayhotchkiss@gmail.com>
2025-03-12 19:40:39 -0700

    Philosopher 0 is greedy

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,94 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:isolate';
 
 void main() {
-  runApp(const MyApp());
+  start();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void start() async {
+  var receivePort = ReceivePort();
+  await Isolate.spawn(coordinator, receivePort.sendPort);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  var coordinatorPort = await receivePort.first as SendPort;
+  List<SendPort> philosopherPorts = [];
+
+  for (int i = 0; i < 5; i++) {
+    var philosopherReceivePort = ReceivePort();
+    await Isolate.spawn(philosopher, philosopherReceivePort.sendPort);
+    var sendPort = await philosopherReceivePort.first as SendPort;
+    philosopherPorts.add(sendPort);
+
+    // Initialize each philosopher with their ID and the coordinator's port
+    sendPort.send({'id': i, 'coordinator': coordinatorPort});
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+void coordinator(SendPort sendPort) {
+  var receivePort = ReceivePort();
+  sendPort.send(receivePort.sendPort);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  List<bool> chopsticks = List.filled(5, true);
+  receivePort.listen((message) {
+    var id = message['id'] as int;
+    var request = message['request'] as String;
+    var responsePort = message['port'] as SendPort;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    if (request == 'pickup') {
+      int left = id;
+      int right = (id + 1) % 5;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+      if (chopsticks[left] && chopsticks[right]) {
+        chopsticks[left] = false;
+        chopsticks[right] = false;
+        responsePort.send(true); // Allow philosopher to eat
+      } else {
+        responsePort.send(false); // Deny access to chopsticks
+      }
+    } else if (request == 'putdown') {
+      chopsticks[id] = true;
+      chopsticks[(id + 1) % 5] = true;
+      responsePort.send(true);
+    }
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+void philosopher(SendPort sendPort) {
+  var receivePort = ReceivePort();
+  sendPort.send(receivePort.sendPort);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  int? id;
+  SendPort? coordinatorPort;
+  receivePort.listen((message) {
+    if (message is Map) {
+      id = message['id'] as int;
+      coordinatorPort = message['coordinator'] as SendPort;
+    } else if (message == true) {
+      print('Philosopher $id is eating.');
+      Future.delayed(Duration(seconds: 2), () {
+        print('Philosopher $id finished eating.');
+        coordinatorPort?.send({
+          'id': id!,
+          'request': 'putdown',
+          'port': receivePort.sendPort,
+        });
+      });
+    } else {
+      print('Philosopher $id is thinking.');
+      Future.delayed(Duration(seconds: 2), () {
+        coordinatorPort?.send({
+          'id': id!,
+          'request': 'pickup',
+          'port': receivePort.sendPort,
+        });
+      });
+    }
+  });
+
+  Future.delayed(Duration.zero, () {
+    coordinatorPort?.send({
+      'id': id!,
+      'request': 'pickup',
+      'port': receivePort.sendPort,
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  });
 }
